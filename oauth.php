7b95348a905f6d6ad5703d2dca52385b2b9e743e
komalghai <komal.3ginfo@gmail.com>
2016-07-25 18:55:45 +0530

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -19,8 +19,8 @@ use phpish\shopify;
 	{
 		$permission_url = shopify\authorization_url($_GET['shop'], SHOPIFY_APP_API_KEY, array('read_content', 'write_content', 'read_themes', 'write_themes', 'read_products', 'write_products', 'read_customers', 'write_customers', 'read_orders', 'write_orders', 'read_script_tags', 'write_script_tags', 'read_fulfillments', 'write_fulfillments', 'read_shipping', 'write_shipping'),'https://bulkdelte.herokuapp.com/');
 	//$permission_url = $permission_url ."&redirect_uri=https://bulkdelte.herokuapp.com/";
-	//echo "<script>alert('$permission_url');</script>";
-		die("<script> top.location.href='$permission_url'</script>");
+	echo "<script>alert('$permission_url');</script>";
+		die("<script> window.location.href='$permission_url'</script>");
 	}
 
 
