46132b1eacb7e3f5210b45937c9eb4152b0ce1b5
komalghai <komal.3ginfo@gmail.com>
2016-08-08 12:56:17 +0530

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 	session_start();
-echo "path=".__DIR__;
+
 	require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 	require __DIR__.'/conf.php';
